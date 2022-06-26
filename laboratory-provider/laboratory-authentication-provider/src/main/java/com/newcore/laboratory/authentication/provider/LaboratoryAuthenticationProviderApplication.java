187fce5284aeb961fabcceba9c401c742af36410
zhouchaowei <zhouchaowei96492@163.com>
2022-06-26 10:26:04 +0800

    14-项目构建-提交认证模块工程

---
+++ laboratory-provider/laboratory-authentication-provider/src/main/java/com/newcore/laboratory/authentication/provider/LaboratoryAuthenticationProviderApplication.java
@@ -0,0 +1,16 @@
+package com.newcore.laboratory.authentication.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 实验室-认证服务-主启动类
+ * @author zhouchaowei
+ */
+@SpringBootApplication
+public class LaboratoryAuthenticationProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratoryAuthenticationProviderApplication.class);
+    }
+}
