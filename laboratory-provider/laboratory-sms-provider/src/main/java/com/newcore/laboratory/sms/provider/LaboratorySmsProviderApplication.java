4863d408bdf0418e9b3ba085015fb299b42a978f
zhouchaowei <zhouchaowei96492@163.com>
2022-06-26 11:09:26 +0800

    18-项目构建-提交短信服务工程

---
+++ laboratory-provider/laboratory-sms-provider/src/main/java/com/newcore/laboratory/sms/provider/LaboratorySmsProviderApplication.java
@@ -0,0 +1,16 @@
+package com.newcore.laboratory.sms.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 实验室-短信服务-主启动类
+ * @author zhouchaowei
+ */
+@SpringBootApplication
+public class LaboratorySmsProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratorySmsProviderApplication.class,args);
+    }
+}
