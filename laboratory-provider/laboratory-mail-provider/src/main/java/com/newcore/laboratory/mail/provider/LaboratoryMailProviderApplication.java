1f9b97ac0734fda46b4335dc170cccb75bb16f4d
zhouchaowei <zhouchaowei96492@163.com>
2022-06-26 10:46:40 +0800

    17-项目构建-提交电邮服务工程

---
+++ laboratory-provider/laboratory-mail-provider/src/main/java/com/newcore/laboratory/mail/provider/LaboratoryMailProviderApplication.java
@@ -0,0 +1,16 @@
+package com.newcore.laboratory.mail.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 实验室-邮件服务-主启动类
+ * @author zhouchaowei
+ */
+@SpringBootApplication
+public class LaboratoryMailProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboratoryMailProviderApplication.class,args);
+    }
+}
